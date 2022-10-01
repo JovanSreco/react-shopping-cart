abf72090fbb190ba08ce46eeebd6b3b906b00cb1
Jovan Sreco <jovan.sreco@gmail.com>
2022-10-01 14:57:44 +0200

    Fetch Items Implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Welcome to a simple shopping cart example, you can navigate to{" "}
+        <Link to="/store">store</Link> to test the application
+      </p>
+    </>
+  );
 };
 
 export default Home;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,56 @@
+import React, { useCallback, useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import StoreItem from "../components/StoreItem";
-import storeItems from "../data/items.json";
+import DUMMY from "../data/items.json";
+
+type storeItemType = {
+  id: number;
+  imgUrl: string;
+  name: string;
+  price: number;
+};
 
 const Store: React.FC = () => {
+  const [storeItems, setStoreItems] = useState<storeItemType[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+  const [error, setError] = useState(null);
+
+  const fetchStoreItemsData = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://react-http-4d4cd-default-rtdb.firebaseio.com/storeItems.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+      let arr = [];
+      for (const key in data) {
+        arr.push(data[key]);
+      }
+
+      console.log(arr);
+      setStoreItems(arr);
+      setIsLoading(false);
+    } catch (error: any) {
+      setError(error);
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchStoreItemsData();
+  }, [fetchStoreItemsData]);
+
+  if (error) {
+    return <p className="text-center">{error}</p>;
+  }
+
+  if (isLoading && !error) {
+    return <p className="text-center">Loading...</p>;
+  }
+
   return (
     <>
       <h1>Store</h1>
